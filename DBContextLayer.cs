99ea9bde30f18d6b83bd3573e5743d24f9b9ad25
Yuvraj <128832039+yuvgit@users.noreply.github.com>
2024-05-31 14:39:50 +0530

    Add files via upload

---
+++ DBContextLayer.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Options;
+using Project_Curd.Models;
+
+namespace Project_Curd
+{
+    public class DBContextLayer : DbContext
+    {
+        public DBContextLayer(DbContextOptions<DBContextLayer>options):base(options) { }
+        
+        public DbSet<TeacherModel> Teachers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TeacherModel>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(50).IsUnicode(false);
+                entity.Property(e=>e.Skills).IsRequired().HasMaxLength(250).IsUnicode(false);
+                entity.Property(e => e.Salary).IsRequired().HasColumnType("money");
+                entity.Property(e => e.AddedOn).HasColumnType("date").HasDefaultValueSql("(getDate())");
+            });
+        }
+        //public class YourDbContextFactory : IDesignTimeDbContextFactory<DBContextLayer>
+        //{
+        //    public DBContextLayer CreateDbContext(string[] args)
+        //    {
+        //        var optionsBuilder = new DbContextOptionsBuilder<DBContextLayer>();
+        //        optionsBuilder.UseSqlServer("DefaultConnection");
+
+        //        return new DBContextLayer(optionsBuilder.Options);
+        //    }
+        //}
+    }
+}
---
+++ Models/TeacherModel.cs
@@ -0,0 +1,23 @@
+﻿using Project_Curd.CustomValidator;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_Curd.Models
+{
+    public class TeacherModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        [SkillsValidate(Allowed= new string[] {"ASP .NET","EF Core","ASP.NET Core","WEB API"},ErrorMessage ="Your skills are invalid")]
+        public string? Skills {  get; set; }
+
+        [Range(50,100)]
+        public int TotalStudents { get; set; }
+        [Required]
+        public int Salary { get; set; }
+        [Required]
+        public DateTime AddedOn  { get; set;}
+    }
+}
