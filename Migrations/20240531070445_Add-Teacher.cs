1fe026edb836080f45bb04b9ed5507acbc8077c9
Yuvraj <128832039+yuvgit@users.noreply.github.com>
2024-05-31 14:37:59 +0530

    Add files via upload

---
+++ Migrations/20240531070445_Add-Teacher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project_Curd.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTeacher : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Skills = table.Column<string>(type: "varchar(250)", unicode: false, maxLength: 250, nullable: false),
+                    TotalStudents = table.Column<int>(type: "int", nullable: false),
+                    Salary = table.Column<decimal>(type: "money", nullable: false),
+                    AddedOn = table.Column<DateTime>(type: "date", nullable: false, defaultValueSql: "(getDate())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
